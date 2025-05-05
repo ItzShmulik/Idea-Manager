38773d6511bb7db547d25b1e3c532ae3ccc62f30
shmulik55 <gshloman@gmail.com>
2025-05-06 00:24:25 +0300

    - Added note add and delete buttons - Fixed buttons container in place

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import plusIcon from "../public/sitcky-note-plus.png";
+import deleteIcon from "../public/sticky-note-delete.png";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="note-button-container">
+        <button className="note-button add-note-button">
+          <img src={plusIcon}></img>
+        </button>
+        <button className="note-button remove-note-button">
+          <img src={deleteIcon}></img>
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
